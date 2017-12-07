818343a557eac2d08a9da588d3e7b613cd9762f9
Eung Jin Lee <oiojin831@gmail.com>
2017-12-07 17:01:01 +0900

    Add Header

---
+++ header.jsx
@@ -0,0 +1,12 @@
+<Fixed top right left bg='main'>
+  <Container>
+    <Flex justify='space-between' align='center' bg='main'>
+      <Box  ml={[3,3,5,3]}>
+        <Logo py={['10px']} src='https://dummyimage.com/120x40/000/fff.png&text=UNDEFINED' />
+      </Box>
+      <Box>
+        <LoginButton mr={[3,3,5,3]}>예약하기</LoginButton>
+      </Box>
+    </Flex>
+  </Container>
+</Fixed>
