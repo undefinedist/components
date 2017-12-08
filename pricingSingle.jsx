755cd994cf9019068220db2cacb4d1bba13a1fcb
Eung Jin Lee <oiojin831@gmail.com>
2017-12-08 17:17:23 +0900

    add two component

---
+++ pricingSingle.jsx
@@ -0,0 +1,35 @@
+<Container>
+  <Flex wrap p={[4,3]}>
+  <ResponsiveBackground p={[3]} src='https://mrmrs.github.io/photos/u/001.jpg'>
+    <Flex w={[1, 1, 1/2]}>
+      <Flex flexDirection='column'>
+        <Text color='green' fontSize={[0,1]}>STAR WARS APPAIR</Text>
+        <Text lineHeight={[1.09]} my={[1]} fontSize={[4,6]}>어차피 물어볼거지만 설명은 있어야겠지?</Text>
+        <Text fontSize={[3,4]} my={[2]}>$400</Text>
+        <Text fontSize={[2]} mb={[3]}>all hala lall ejfie eungeuif fjie fjie abcdfjf ejaif jcivneojo dfjodfjoa jojdfijall half=pant againg blablalbalbal bla bla balb al blablalbalbla ba bal</Text>
+        <Flex my={[3]}>
+          <Flex w={[2/5]} flexDirection='column'>
+            <Text fontSize={[2]}>가격</Text>
+            <Text fontSize={[2]}>tdjffj</Text>
+            <Text fontSize={[2]}>hello</Text>
+            <Text fontSize={[2]}>hello</Text>
+            <Text fontSize={[2]}>hello</Text>
+          </Flex>
+          <Flex w={[3/5]} flexDirection='column'>
+            <Text fontSize={[2]}>hello</Text>
+            <Text fontSize={[2]}>hello</Text>
+            <Text fontSize={[2]}>hello</Text>
+            <Text fontSize={[2]}>hello</Text>
+            <Text fontSize={[2]}>hello</Text>
+          </Flex>
+        </Flex>
+        <Box>
+          <ButtonOutline bg='white' color='green' fontSize={[0]} py={[3]} px={[5]} >BUTTON</ButtonOutline>
+        </Box>
+      </Flex>
+    </Flex>
+    <Flex w={[0, 0, 1/2]}>
+    </Flex>
+    </ResponsiveBackground>
+  </Flex>
+</Container>
