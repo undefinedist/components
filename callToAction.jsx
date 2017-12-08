276dcab6fe1671b105fdb6b2685e2e1cee5b5ae7
Eung Jin Lee <oiojin831@gmail.com>
2017-12-08 16:07:00 +0900

    add calltomargin

---
+++ callToAction.jsx
@@ -0,0 +1,7 @@
+<Container>
+  <Flex bg='green' py={[5]} flexDirection='column' justify='center' align='center'>
+    <Text color='white' align='center' fontSize={[2]}>HOT SALE</Text>
+    <Text pt={[2]} pb={[1]} color='white' align='center' fontSize={[4, 6]}>Don't Miss This Deal</Text>
+    <Button bg='white' color='green' fontSize={[0]} py={[3]} px={[5]} >BUTTON</Button>
+  </Flex>
+</Container>
